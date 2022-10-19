29f1a3711627ab126daa4a6c54972c13e28ceb71
t.chambard <t.chambard@bee-buzziness.com>
2022-10-20 00:59:30 +0200

    install eth95

---
+++ packages/rinkeby-sandbox-contracts/contracts/exercices/SimpleStoragePayable.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0 
+
+pragma solidity 0.8.17; 
+
+contract SimpleStoragePayable { 
+    uint value;
+
+    constructor() payable {
+
+    }
+    
+    function get() external view returns(uint) {
+        return value;
+    }
+    
+    function set(uint n) external {
+        value = n;
+    }
+    
+} 
