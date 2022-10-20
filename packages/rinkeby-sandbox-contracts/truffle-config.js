8691b3250f2ae04586561c3db9abcafc122e8826
t.chambard <t.chambard@bee-buzziness.com>
2022-10-20 22:05:43 +0200

    Add eth-gas-reporter (disabled)

--- packages/rinkeby-sandbox-contracts/truffle-config.js
+++ packages/rinkeby-sandbox-contracts/truffle-config.js
@@ -86,13 +86,13 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${INFURA_ID}`),
+      network_id: 5,       // Goerli's id
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
@@ -105,6 +105,10 @@ module.exports = {
   // Set default mocha options here, use special reporters, etc.
   mocha: {
     // timeout: 100000
+    // reporter: 'eth-gas-reporter',
+    // reporterOptions: {
+    //   excludeContracts: ['Migrations']
+    // }
   },
 
   // Configure your compilers
