d52f6047f717031fa065aa84c4010e3295ca2e5d
t.chambard <t.chambard@bee-buzziness.com>
2022-10-20 14:05:04 +0200

    Unit tests for AlyraERC20Token

--- packages/rinkeby-sandbox-contracts/test/AlyraERC20Token.spec.ts
+++ packages/rinkeby-sandbox-contracts/test/AlyraERC20Token.spec.ts
@@ -1,4 +1,4 @@
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { BN } from '@openzeppelin/test-helpers';
 
 import { AlyraERC20TokenInstance } from '../types/truffle/AlyraERC20Token';
@@ -36,19 +36,53 @@ contract('AlyraERC20Token', accounts => {
   });
 
   it('check balance after transfer', async () => {
-    let amount = BN(100);
-    let balanceOwnerBeforeTransfer = await instance.balanceOf(_owner);
-    let balanceRecipientBeforeTransfer = await instance.balanceOf(_recipient)
+    const amount = 100;
+    const balanceOwnerBeforeTransfer = await instance.balanceOf(_owner);
+    const balanceRecipientBeforeTransfer = await instance.balanceOf(_recipient)
 
     assert.equal(balanceRecipientBeforeTransfer.toNumber(), 0);
     
-    await instance.transfer(_recipient, BN(100), { from: _owner });
+    await instance.transfer(_recipient, 100, { from: _owner });
 
-    let balanceOwnerAfterTransfer = await instance.balanceOf(_owner);
-    let balanceRecipientAfterTransfer = await instance.balanceOf(_recipient)
+    const balanceOwnerAfterTransfer = await instance.balanceOf(_owner);
+    const balanceRecipientAfterTransfer = await instance.balanceOf(_recipient)
 
-    assert.equal(balanceOwnerAfterTransfer.toNumber(), balanceOwnerBeforeTransfer.sub(amount).toNumber());
-    assert.equal(balanceRecipientAfterTransfer.toNumber(), balanceRecipientBeforeTransfer.add(amount).toNumber());
+    assert.equal(balanceOwnerAfterTransfer.toNumber(), balanceOwnerBeforeTransfer.sub(BN(amount)).toNumber());
+    assert.equal(balanceRecipientAfterTransfer.toNumber(), balanceRecipientBeforeTransfer.add(BN(amount)).toNumber());
+    
+  });
+
+  it('check balance after approve', async () => {
+    const amount = 100;
+    const allowanceBeforeApprove = await instance.allowance(_owner, _recipient);
+
+    assert.equal(allowanceBeforeApprove.toNumber(), 0);
+    
+    await instance.approve(_recipient, amount, { from: _owner });
+
+    const allowanceAfterApprove = await instance.allowance(_owner, _recipient);
+    assert.equal(allowanceAfterApprove.toNumber(), amount);
+    
+  });
+
+  it('check transferFrom after approve', async () => {
+    const amount = 100;
+
+    await instance.approve(_recipient, amount);
+
+    const balanceOwnerBeforeTransferFrom = await instance.balanceOf(_owner);
+    const balanceRecipientBeforeTransferFrom = await instance.balanceOf(_recipient);
+
+    assert.equal(balanceOwnerBeforeTransferFrom.toNumber(), _initialSupply);
+    assert.equal(balanceRecipientBeforeTransferFrom.toNumber(), 0);
+    
+    await instance.transferFrom(_owner, _recipient, amount, { from: _recipient });
+    
+    const balanceOwnerAfterTransferFrom = await instance.balanceOf(_owner);
+    const balanceRecipientAfterTransferFrom = await instance.balanceOf(_recipient);
+
+    assert.equal(balanceOwnerAfterTransferFrom.toNumber(), _initialSupply - 100);
+    assert.equal(balanceRecipientAfterTransferFrom.toNumber(), 100);
     
   });
 
